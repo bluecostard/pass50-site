f445fb6ba02051fa1adfb120983140b6798a127c
bluecostard <elvii86@yahoo.fr>
2026-08-06 17:03:39 +0200

    Activer le tableau PASS50 Intelligence V2

--- api/intelligence.php
+++ api/intelligence.php
@@ -2,7 +2,8 @@
 declare(strict_types=1);
 require __DIR__.'/bootstrap.php';
 require __DIR__.'/intelligence-core.php';
+require __DIR__.'/intelligence-dashboard-v2.php';
 require_method('GET');
 $user=auth_user();
 require_role($user,'owner','admin');
-json_response(['ok'=>true]+p50_intelligence_dashboard());
+json_response(['ok'=>true]+p50_intelligence_dashboard_v2());
